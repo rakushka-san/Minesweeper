d2c53e6ec40b51e213a21050e161e0336834ee39
rakushka-san <vladislavmohov@mail.ru>
2023-04-12 02:47:30 +0300

    Определение положения мин

--- main.js
+++ main.js
@@ -6,4 +6,22 @@ $(document).ready(function() {
             class: 'grid-item'
         }));
     }
+
+    const chance = 0.33;
+    let mines = [];
+
+    for (let index = 0; index < side * side; index++) {
+        if (Math.random() <= chance) {
+            mines.push({
+                y: Math.floor(index / 10),
+                x: index % 10,
+            });
+
+            // для наглядности
+            $('.grid-item:eq(' + index + ')').addClass('revealed');
+        }
+    }
+
+    console.log(mines);
+    $('#minesLeft').html(mines.length);
 });
